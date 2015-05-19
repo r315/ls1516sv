80c4973589b74bc32f7e6a7eff68fa2847c9d3dd
Pedro Felix <pmhsfelix@gmail.com>
2015-05-19 11:26:32 +0100

    Trying to deploy on heroku

---
+++ src/main/java/pt/isel/ls/apps/http/HerokuFirstHttpServer.java
@@ -0,0 +1,17 @@
+package pt.isel.ls.apps.http;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
+
+public class HerokuFirstHttpServer {
+    
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new TimeServlet()),"/*");
+        server.start();
+        server.join();
+    }  
+}
