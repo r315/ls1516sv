a7491c8a5dd2446478b273d42a1ceaa3a57cbfc9
r315 <mreishugo@gmail.com>
2016-03-07 19:04:22 +0000

    Aula 07-03-2016

--- src/main/java/pt/isel/ls/Ints.java
+++ src/main/java/pt/isel/ls/Ints.java
@@ -16,10 +16,10 @@ public class Ints {
         if (r>=v.length) {
             throw new IllegalArgumentException("finish index cannot be greater than the length of the array");
         }
-        for (int i = l+1; i < r; ++i) {
+        for (int i = l+1; i <= r; ++i) {
             int key = v[i];
             int j = i - 1;
-            for (; j > 0 && v[j] > key; --j)
+            for (; j >= 0 && v[j] > key; --j)
                 v[j + 1] = v[j];
             v[j + 1] = key;
         }
