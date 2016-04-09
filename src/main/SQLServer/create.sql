603d0ab9d38cb1eed3de3b46b3186d2f5259d162
lsalmeida <a36187@alunos.isel.pt>
2016-04-09 21:57:03 +0100

    Added RI to create.sql

--- src/main/SQLServer/create.sql
+++ src/main/SQLServer/create.sql
@@ -5,6 +5,7 @@ CREATE TABLE Movie(
 	genre varchar(200),
 	cast_directors varchar(45),
 	cover binary NULL,
+	CONSTRAINT movieDate UNIQUE (title,release_year)
 )
 
 CREATE TABLE Review(
@@ -14,17 +15,19 @@ CREATE TABLE Review(
 	review varchar(255) NOT NULL,
 	summary varchar(200) NOT NULL,
 	rating int NOT NULL,
-	PRIMARY KEY(review_id, movie_id)
+	PRIMARY KEY(review_id, movie_id),
+	CHECK (rating>=0 AND rating<=5)
 )
 
 CREATE TABLE Rating(
 	rating_id int IDENTITY(1,1),
 	movie_id int FOREIGN KEY REFERENCES Movie(movie_id),
-	one INT NULL,
-	two INT NULL,
-	three INT NULL,
-	four INT NULL,
-	five INT NULL,
+	one INT NOT NULL,
+	two INT NOT NULL,
+	three INT NOT NULL,
+	four INT NOT NULL,
+	five INT NOT NULL,
 	PRIMARY KEY(rating_id, movie_id)
+	CHECK (one>=0 AND two>=0 AND three>=0 AND four>=0 AND five>=0)
 )
 
