390ca0401feaa1155fbe53376aba1c1bf31478e0
r315 <mreishugo@gmail.com>
2016-04-27 00:58:17 +0100

    Html Result
    start creating tree

--- src/main/java/Strutures/HtmlResult.java
+++ src/main/java/Strutures/HtmlResult.java
@@ -6,9 +6,93 @@ import java.util.Collection;
  * Created by Red on 24/04/2016.
  */
 public class HtmlResult implements IResult {
+	
 
     //TODO
     public void display(ResultInfo resultInfo){
+    	HtmlNode root = new HtmlNode("html");
+    	
+    	HtmlTree.insert(root,new HtmlNode("head"));
+    	
+    	String t = root.getHtml();
 
     }
+    
+    
+      
+    public void writeToFile(String filename){
+    	
+    }
+    
+    
+    
+    
+    
+    class HtmlNode{
+    	String tag = "";
+    	String content ="";
+    	HtmlNode next = null;
+    	
+    	public HtmlNode(String tag){
+    		this.tag = tag;
+    	}
+    	
+    	public void addContent(String content){
+    		this.content = content;
+    	}
+    	
+    	public String getHtml(){
+    		return "<" + tag +">" + (next != null? next.getHtml():"") + content + "</" + tag + ">";
+    	}
+    } 
+    
+    static class HtmlTree{
+    	
+    	public static void insert(HtmlNode root, HtmlNode node){
+    		if(root.next == null){
+    			root.next = node;
+    			return;
+    		}    		
+    		insert(root.next,node);
+    	}
+    }
+    
+    
 }
+
+
+/*
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Titulo tab browser </title> 
+		<h2> Titulo pagina </h2>
+		<style> table, th, td { border: 1px solid black; border-collapse: collapse;} </style>
+	</head>
+	<body>
+		<table style="width:100%">
+			<tr>
+				<th>Titulo</th>
+				<th>Ano de Lancamento</th>		
+				<th>Review</th>
+			</tr>
+		   	<tr>
+				<td>Jill</td>
+				<td>Smith</td>		
+				<td>50</td>
+		  	</tr>
+		  	<tr>
+				<td>Eve</td>
+				<td>Jackson</td>		
+				<td>94</td>
+		  	</tr>
+		  	<tr>
+				<td>John</td>
+				<td>Doe</td>		
+				<td>80</td>
+		  	</tr>
+		</table>
+	</body>
+</html>
+*/
+
---
+++ src/test/java/structures/HtmlResultTest.java
@@ -0,0 +1,19 @@
+package structures;
+
+import org.junit.Test;
+
+import Strutures.HtmlResult;
+
+public class HtmlResultTest {
+	
+	
+	@Test
+	public void createHtmlwithTitle(){
+		
+		
+		HtmlResult hr = new HtmlResult();
+		hr.display(null);
+	}
+	
+
+}
