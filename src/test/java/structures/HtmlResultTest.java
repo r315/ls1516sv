9bfea2df5a7119b52bb0d91f61ad4a3c5d34aac3
r315 <mreishugo@gmail.com>
2016-04-27 23:07:17 +0100

    HtmlResult release

--- src/test/java/structures/HtmlResultTest.java
+++ src/test/java/structures/HtmlResultTest.java
@@ -1,19 +1,49 @@
 package structures;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import Strutures.HtmlResult;
+import Strutures.ResultInfo;
 
 public class HtmlResultTest {
+	public static HtmlResult hr;
 	
-	
-	@Test
-	public void createHtmlwithTitle(){
-		
-		
-		HtmlResult hr = new HtmlResult();
-		hr.display(null);
+	@BeforeClass
+	public static void createHtmlResultInstance(){
+		hr = new HtmlResult();
 	}
 	
+	@Test
+	public void createHtml(){		
+		hr.display(createResultInfo());
+	}
+	
+	@Test
+	public void saveHtmlToFile() throws FileNotFoundException{		
+		createHtml();
+		hr.writeToFile("HtmlOut.html");		
+	}
+	
+	
+	private ResultInfo createResultInfo(){
+		
+		 ArrayList<String> columns = new ArrayList<>();
+		 ArrayList<ArrayList<String>> data = new ArrayList<>();
+		 
+		 for(int i = 0; i<4;i++){
+			 columns.add("Column Title "+(i+1));
+			 ArrayList<String> line = new ArrayList<String>();
+			 line.add("Line "+i);
+			 data.add(line);
+		 }
+		 return new ResultInfo("Teste",columns,data);
+		
+	}
 
 }
