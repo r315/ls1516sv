cd7ef9c23abf44e351664deca3d10258addda8c3
lsalmeida <a36187@alunos.isel.pt>
2016-05-27 22:45:24 +0100

    Added Pair class

---
+++ src/main/java/utils/Pair.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ * Created by Luigi Sekuiya on 24/05/2016.
+ */
+public class Pair<T,U> {
+    public T value1;
+    public U value2;
+
+    public Pair(T value1, U value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+}
