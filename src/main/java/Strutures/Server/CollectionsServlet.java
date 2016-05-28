3f34a55c2662a532dcba838fb99456974eeda131
redice524 <redice_dragon@hotmail.com>
2016-05-28 02:54:10 +0100

    bla

--- src/main/java/Strutures/Server/CollectionsServlet.java
+++ src/main/java/Strutures/Server/CollectionsServlet.java
@@ -36,24 +36,34 @@ public class CollectionsServlet extends HttpServlet{
             HeaderInfo headerInfo = new HeaderInfo(new String[]{});
             CommandInfo command = new CommandInfo(new String[]{req.getMethod(),req.getRequestURI()});
             HtmlResult resultFormat= (HtmlResult)Manager.executeCommand(command,headerInfo);
+
+
+
+
+
+
+
+            //Add collections links to each column
+            List<Pair<String,String>> pairs=new ArrayList<>();
+
+            for (ArrayList<String> line : resultFormat.resultInfo.getValues())
+                pairs.add(new Pair<>(line.get(1),"/collections/"+line.get(0)));
+
+
+            //Remove id column
+            List<String> list= resultFormat.resultInfo.removeColumn("ID");
+
             resultFormat.generate();
 
             //Set Navigation links
             resultFormat.addNavigationLinks(
-                Arrays.asList(
-                    new Pair<>("Home","/")
-                )
+                    Arrays.asList(
+                            new Pair<>("Home","/")
+                    )
             );
 
-            //Remove id column
-            List<String> list= resultFormat.resultInfo.removeColumn("id");
-
-            //Add collections links to each column
-            List<Pair<String,String>> pairs=new ArrayList<>();
-            for (String s: list)
-                pairs.add(new Pair<>("name","/collections/"+s));
             resultFormat.addLinksToTable(pairs);
-
+            respBody=resultFormat.getHtml();
         }catch(Exception e){
             //// TODO: 19/05/2016
             resp.setStatus(404);
