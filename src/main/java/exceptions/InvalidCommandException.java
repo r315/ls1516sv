4f1a4041ceb12a46155940d3b58a64c781ed9d7e
redice524 <redice_dragon@hotmail.com>
2016-06-18 14:52:13 +0100

    Created new exception InvalidCommandException which all the command related exceptions extend.

---
+++ src/main/java/exceptions/InvalidCommandException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+/**
+ * Created by Red on 18/06/2016.
+ */
+public class InvalidCommandException extends Exception{
+    public InvalidCommandException(String s){super(s);}
+}
